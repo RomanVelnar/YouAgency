946395a0293ba95461e4e3e65d3abb4c350896e6
RomanVelnar <roman.velnar@gmail.com>
2022-06-18 13:35:16 +0100

    refactored Destinations section

---
+++ app.js
@@ -0,0 +1,7 @@
+// function findScroller(element) {
+//     element.onscroll = function() { console.log(element)}
+
+//     Array.from(element.children).forEach(findScroller);
+// }
+
+// findScroller(document.body);
