0e634b0e9c5e0ca59805cb4f80f482086b44ddba
RomanVelnar <roman.velnar@gmail.com>
2022-06-19 18:27:31 +0100

    last code cleanUp and added Table Of Contents

--- app.js
+++ app.js
@@ -1,31 +1,32 @@
-// JS to find vertical scroll
-// function findScroller(element) {
-//     element.onscroll = function() { console.log(element)}
+// Swiper for Hero
 
-//     Array.from(element.children).forEach(findScroller);
-// }
-
-// findScroller(document.body);
-
-// JS to find horizontal scroll
-// var docWidth = document.documentElement.offsetWidth;
-
-// [].forEach.call(
-//   document.querySelectorAll('*'),
-//   function(el) {
-//     if (el.offsetWidth > docWidth) {
-//       console.log(el);
-//     }
-//   }
-// );
-
-// JS for overflow
-
-// const isOverflown = ({ clientWidth, clientHeight, scrollWidth, scrollHeight }) => {
-//     return scrollHeight > clientHeight || scrollWidth > clientWidth;
-// }
-
-
+var swiper = new Swiper(".swiper-hero", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 0,
+      },
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 6,
+        spaceBetween: 50,
+      },
+    },
+  });
+  
 // Swiper JS - Destinations
 
 var swiper = new Swiper(".swiper-destinations", {
@@ -80,32 +81,4 @@ var swiper = new Swiper(".swiper-thingsToDo", {
     },
   });
 
-// Swiper for Hero
-
-var swiper = new Swiper(".swiper-hero", {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 2,
-        spaceBetween: 0,
-      },
-      640: {
-        slidesPerView: 2,
-        spaceBetween: 20,
-      },
-      768: {
-        slidesPerView: 3,
-        spaceBetween: 40,
-      },
-      1024: {
-        slidesPerView: 6,
-        spaceBetween: 50,
-      },
-    },
-  });
 
