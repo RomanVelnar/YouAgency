3e34df04df091aa2f193f4ad5944681305177acf
RomanVelnar <roman.velnar@gmail.com>
2022-06-19 17:23:51 +0100

    added swiper for Things To Do and HeroNav + smalled UI changes

--- app.js
+++ app.js
@@ -28,7 +28,36 @@
 
 // Swiper JS - Destinations
 
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".swiper-destinations", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-buttons-destination-next",
+        prevEl: ".swiper-buttons-destination-prev",
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 6,
+        spaceBetween: 50,
+      },
+    },
+  });
+
+// Swiper for Things to do
+
+var swiper = new Swiper(".swiper-thingsToDo", {
     slidesPerView: 1,
     spaceBetween: 10,
     navigation: {
@@ -50,3 +79,33 @@ var swiper = new Swiper(".mySwiper", {
       },
     },
   });
+
+// Swiper for Hero
+
+var swiper = new Swiper(".swiper-hero", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 0,
+      },
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 6,
+        spaceBetween: 50,
+      },
+    },
+  });
+
