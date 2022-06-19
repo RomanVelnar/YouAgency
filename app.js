972e7b9ea3dd1f148e7ce4092edd5711eb3808f9
RomanVelnar <roman.velnar@gmail.com>
2022-06-19 13:27:16 +0100

    removed the code that caused horizontal overflow

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// JS to find vertical scroll
 // function findScroller(element) {
 //     element.onscroll = function() { console.log(element)}
 
@@ -5,3 +6,47 @@
 // }
 
 // findScroller(document.body);
+
+// JS to find horizontal scroll
+// var docWidth = document.documentElement.offsetWidth;
+
+// [].forEach.call(
+//   document.querySelectorAll('*'),
+//   function(el) {
+//     if (el.offsetWidth > docWidth) {
+//       console.log(el);
+//     }
+//   }
+// );
+
+// JS for overflow
+
+// const isOverflown = ({ clientWidth, clientHeight, scrollWidth, scrollHeight }) => {
+//     return scrollHeight > clientHeight || scrollWidth > clientWidth;
+// }
+
+
+// Swiper JS - Destinations
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 6,
+        spaceBetween: 50,
+      },
+    },
+  });
